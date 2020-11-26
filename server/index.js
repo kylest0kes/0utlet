16ebbd4c6814ea52362e79f5a7165015846964c2
kylest0kes <kylestokes1551@gmail.com>
2020-11-25 19:40:24 -0500

    set up server file

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://outletadmin:outletadmin@cluster0.oh3t9.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3001;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
