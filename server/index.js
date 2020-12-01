b9ffe7275402feddc6e9cb16d6a8df0a89cbd852
kylest0kes <kylestokes1551@gmail.com>
2020-11-30 19:00:04 -0500

    connected express on local host 3001

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from './Routes/posts-routes.js';
 
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
@@ -13,7 +16,7 @@ const CONNECTION_URL = 'mongodb+srv://outletadmin:outletadmin@cluster0.oh3t9.mon
 const PORT = process.env.PORT || 3001;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .then(() => app.listen(PORT, () => console.log(`Sweet! Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
