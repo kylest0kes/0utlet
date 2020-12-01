fc463f58b7bf5f0c9db124812ecfec5fd092efc4
kylest0kes <kylestokes1551@gmail.com>
2020-11-30 22:08:47 -0500

    created modal button and pop up to be used for posting

--- client/src/components/postmodal/index.js
+++ client/src/components/postmodal/index.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../images/logo.png';
 
 export default function PostModal() {
     return (
         <div>
-            <h1>modal</h1>
+            <button type="button" className="btn post-btn" data-toggle="modal" data-target="#postModal">
+                <img className="btn-img" src={logo} alt="logo" />
+            </button>
+
+            {/* MODAL */}
+            <div className="modal fade" id="postModal" tabindex="-1" aria-labelledby="postModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="postModalLabel">Modal title</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        ...
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
